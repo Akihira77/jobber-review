8eb11c50c6499c98e58f7c783a8729ba6d5b67ea
Akihira77 <tik.dikawahyu123@gmail.com>
2024-04-01 12:48:06 +0700

    Refactore code from previous test

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,11 @@
 import { Application } from "express";
 import { healthRoutes } from "@review/routes/health.route";
 import { reviewRoutes } from "@review/routes/review.route";
-// import { verifyGatewayRequest } from "@Akihira77/jobber-shared";
+import { verifyGatewayRequest } from "@Akihira77/jobber-shared";
 
 const BASE_PATH = "/api/v1/review";
 
 export function appRoutes(app: Application): void {
     app.use("", healthRoutes());
-    // app.use(BASE_PATH, verifyGatewayRequest, reviewRoutes());
-    app.use(BASE_PATH, reviewRoutes());
+    app.use(BASE_PATH, verifyGatewayRequest, reviewRoutes());
 }
